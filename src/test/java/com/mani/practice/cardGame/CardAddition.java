2dd2378a1f09d37e63c0be439b7a953ef0fccb53
mani3705 <manisha.gupta@hotmail.com>
2020-02-24 20:35:26 -0500

    Added a java class

---
+++ src/test/java/com/mani/practice/cardGame/CardAddition.java
@@ -0,0 +1,23 @@
+package com.mani.practice.cardGame;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class CardAddition {
+    int total = 52;
+    List<Integer> cards;
+
+    public void setCards(Player player) {
+        cards = new ArrayList<Integer>();
+        for(int j=1;j<=2;j++) {
+            for (int i = 1; i <= 13; i++) {
+                cards.add(i);
+            }
+        }
+        Collections.shuffle(cards);
+        player.setDeckOfCards(cards);
+
+    }
+}
